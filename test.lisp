a10700715c22ab98c9251e8bc837103abc3e9d7a
cxxxr <g23tlm@gmail.com>
2017-07-10 23:58:24 +0900

    fix package name

--- test.lisp
+++ test.lisp
@@ -1,9 +1,9 @@
 (defpackage :shen-test
-  (:use :cl :shen.misc)
+  (:use :cl :shen.prelude)
   (:export :test))
 (in-package :shen-test)
 
 (defun test ()
-  (shen.install:prelude)
+  (init)
   (with-shen ()
     (shen::|load| "testsuite.shen")))
